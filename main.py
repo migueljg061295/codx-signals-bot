bd10e1e60f7ba961055e37f2f348dd92e10c81cb
migueljg061295 <migueljg061295@gmail.com>
2025-11-20 13:49:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ CHANNEL_FREE = os.getenv("CHANNEL_FREE")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = int(os.getenv("DB_PORT", 5432))
 DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
